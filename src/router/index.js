81fc2a4f5174700a64b107da966e9217b20fa36c
Lwei <654074610@qq.com>
2020-03-17 14:46:09 +0800

    “完成项目的搭建

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+//导入vue
+import Vue from 'vue'
+//导入vue-router
+import VueRouter from 'vue-router'
+//使用VueRouter
+Vue.use(VueRouter)
+//创建一个新的vue-router对象
+var router = new VueRouter({
+    
+    routes: [
+    
+    ]
+})
+//将新的对象暴露出去
+export default router
