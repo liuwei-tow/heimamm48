840e45b40196b8a6739dde9b688a86cbe760ca25
Lwei <654074610@qq.com>
2020-03-25 23:35:25 +0800

    完成登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,17 +7,17 @@
         <div class="titleline"></div>
         <div class="titlelogin">用户登录</div>
       </div>
-      <el-form class="loginform" ref="form" :model="form" label-width="0px">
+      <el-form :rules="rules" class="loginform" ref="form" :model="form" label-width="0px">
         <el-form-item>
-          <el-input prefix-icon="el-icon-user" placeholder="请输入手机号" v-model="form.name"></el-input>
+          <el-input prefix-icon="el-icon-user" placeholder="请输入手机号" v-model="form.phone"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input prefix-icon="el-icon-lock" placeholder="请输入密码" v-model="form.name"></el-input>
+        <el-form-item prop="password">
+          <el-input prefix-icon="el-icon-lock" placeholder="请输入密码" v-model="form.password"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="logincode">
           <el-row>
             <el-col :span="16">
-              <el-input prefix-icon="el-icon-key" placeholder="请输入验证码" v-model="form.name"></el-input>
+              <el-input  prefix-icon="el-icon-key" placeholder="请输入验证码" v-model="form.logincode"></el-input>
             </el-col>
             <el-col :span="8">
               <img class="loginimg" src="../../assets/login_captcha.png" alt />
@@ -25,16 +25,20 @@
           </el-row>
         </el-form-item>
 
-        <el-form-item class="checkboxheight">
-          <el-checkbox v-model="form.type" >
+        <el-form-item class="checkboxheight" prop="isCheck">
+          <el-checkbox v-model="form.isCheck" >
             我已阅读并同意
             <el-link type="primary">用户协议</el-link>和
             <el-link type="primary">隐私条款</el-link>
           </el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">立即创建</el-button>
-          <el-button>取消</el-button>
+          <el-button class="loginbtn" type="primary" @click="onSubmit">登录</el-button>
+          
+        </el-form-item>
+        <el-form-item>
+          
+          <el-button class="loginbtn" type="primary">注册</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -47,14 +51,42 @@ export default {
   data() {
     return {
       form: {
-        name: "",
-        type: []
+        phone:'',
+        password:'',
+        logincode:'',
+        isCheck:[]
+      },
+      rules: {
+          password: [
+            //非空
+            { required: true, message: '密码不能为空', trigger: 'blur' },
+            //长度
+            { min: 5, max: 10, message: '长度在 5 到 10 个字符', trigger: 'blur' }
+          ],
+          logincode:[
+            //非空
+            { required: true, message: '验证码不能为空', trigger: 'blur' },
+            //长度
+            { min: 4, max: 4, message: '验证码的长度为4', trigger: 'blur' }
+          ],
+          isCheck:[
+            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+          ]
       }
     };
   },
   methods: {
     onSubmit() {
-      console.log("submit!");
+      this.$refs.form.validate((valid)=>{
+        if(valid){
+          this.$message({
+          message: '验证成功！',
+          type: 'success'
+        });
+        }else{
+          this.$message.error('验证失败！');
+        }
+      })
     }
   }
 };
@@ -122,6 +154,9 @@ export default {
           line-height: 16px;
         }
         
+      }
+      .loginbtn {
+        width: 100%;
       }
     }
   }
